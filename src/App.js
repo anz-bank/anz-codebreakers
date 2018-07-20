b110a21163f95474610d8fbf76cd68c1b8315c62
Oliver Gassman <oliver.gassman@anz.com>
2018-07-20 12:43:49 +1000

    feat(oceanblue)
    committing after the uplifts to publishing ocean blue components so I can work on this from home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Masthead from '@anz/masthead'
+import Footer from '@anz/footer'
 
 const App = () => (
   <div>
-    <h1>ANZ Codebreakers</h1>
+    <Masthead
+      hero={{
+        title: 'ANZ Codebreakers',
+        subtitle: 'Go Girl Go 4 IT!',
+        image: 'https://placeimg.com/1000/480/tech',
+        show: true
+      }}
+    />
+    <Footer />
   </div>
 )
 
